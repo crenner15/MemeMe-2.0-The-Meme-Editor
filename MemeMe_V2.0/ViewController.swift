a8ccb3477d74ef7c09e52040d4f095822c5f7bc2
Chad Renner <chadrenner@rocketmail.com>
2016-08-09 12:23:05 -0400

    Initial Commit

---
+++ MemeMe_V2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe_V2.0
+//
+//  Created by Chad Renner on 8/9/16.
+//  Copyright © 2016 Chad Renner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
